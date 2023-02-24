7e341932d2884fb06a007e95e3e78f7ccf84ff0c
Bayo Obafemi <ebenezerbayo@gmail.com>
2023-02-24 08:46:23 +0100

    added the foodbag cart project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./cart.css";
+import "./cartAndroid.css";
+// import Button from "./Component/Button";
+// import Menu from "./Component/Menu";
+import Cart from "./Component/Cart";
+import Verification from "./Component/Verification";
+import OrderSummary from "./Component/OrderSummary";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Cart />} />
+        <Route path="verification" element={<Verification />} />
+        <Route path="/verification/ordersuccess" element={<OrderSummary />} />
+      </Routes>
     </div>
   );
 }
